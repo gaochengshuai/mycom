ded41f7e7fcde5080520fd03366133835785adce
gaochengshuai <18636180181@163.com>
2021-09-17 21:55:53 +0800

    通过window启动对应的jar方法 命令如下：到对应的jar下  java -cp 类全路径 参数1 参数2

--- main/src/main/java/com/gaocs/ControllerApplication.java
+++ main/src/main/java/com/gaocs/ControllerApplication.java
@@ -1,5 +1,6 @@
 package com.gaocs;
 
+import com.gaocs.utils.Autosend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControllerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ControllerApplication.class, args);
+		if(args.length < 2){
+			System.out.println("参数不足");
+		}
+		Autosend autosend = new Autosend();
+		autosend.start(args);
+//		SpringApplication.run(ControllerApplication.class, args);
 	}
 
 }
